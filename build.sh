82133c8fe2aa364a8928553c6fa22ad709d59c64
William Jackson <william@subtlecoolness.com>
2021-02-03 10:49:37 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-# apk add --no-cache gcc musl-dev
+apk add --no-cache gcc musl-dev
 
 # build
-pip wheel -vv --no-deps --requirement requirements.txt
+pip wheel -vvv --no-deps --requirement requirements.txt
